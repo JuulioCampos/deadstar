fc23ba388620a6f5cb37a4d02a451a885d57b9b1
Julio Campos <39494748+JuulioCampos@users.noreply.github.com>
2020-03-02 11:39:40 -0300

    estilizando menus e criando menus

--- index.php
+++ index.php
@@ -1,49 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once "header.php" ?>
+<link rel="stylesheet" href="assets/index.css">
+<nav class="navbar navbar-dark bg-dark">
+    <div class="indent">
+      <a class="navbar-brand" href="#">
+        <img src="img\deadstar\logo 1 t.png" width="30" height="30" class="d-inline-block align-top" alt="deadstar logo">
+        deadstar cheats
+      </a>
+    </div>
+    </nav>
 
 <body style="background-color: #7910A8;">
 
-<?php require_once "navbar.php" ?>
+  <div class="indent"> 
+    <div class="container">    
+        <div class="row">
 
-  <div class="container">    
-      <div class="row">
+          <div class="card-login">
+            <div class="card">
+              <div class="card-header">
+                Login
+              </div>
+              <div class="card-body">
+                <form action="auth.php" method="post">
+                  <div class="form-group">
+                    <input name="login-user" type="text" class="form-control" placeholder="user">
+                  </div>
+                  <div class="form-group">
+                    <input name="pass-user" type="password" class="form-control" placeholder="password">
+                  </div>
+                  <div class="check-box">
+                    <input type="checkbox" id="remeber" name="remember" class="remember"><span>remember me</span>
+                  </div>
 
-        <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login
-            </div>
-            <div class="card-body">
-              <form action="auth.php" method="post">
-                <div class="form-group">
-                  <input name="login-user" type="text" class="form-control" placeholder="user">
-                </div>
-                <div class="form-group">
-                  <input name="pass-user" type="password" class="form-control" placeholder="password">
-                </div>
+                  <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
 
-                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
+                  <div class="text-danger" style="text-align: center;font-family: 'Darker Grotesque' ;">
+                    Usuário ou senha inválido(s)
+                  </div>
 
-                <div class="text-danger" style="text-align: center;">
-                  Usuário ou senha inválido(s)
-                </div>
+                  <?php } ?>
+                  <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2'){?>
 
-                <?php } ?>
-                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro2'){?>
-
-                 <div class="text-danger" style="text-align: center;">
-                     Você não está logado!
-                 </div>
+                  <div class="text-danger" style="text-align: center; font-family: 'Darker Grotesque' ;">
+                      Você não está logado!
+                  </div>
 
 
-                <?php } ?>
+                  <?php } ?>
 
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
+                  <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+                  <div class="user-register">
+                    <span name="forgot"><a href="/forgot"> Forgot password</a> </i></span>
+                    <span name="register"><a href="/register"> Register</a> </i></span>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-    </div>
-  </body>
+      </div>
+  </div>
+    </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,6 +1,29 @@
+
 <nav class="navbar navbar-dark bg-dark">
+    <div class="indent">
       <a class="navbar-brand" href="#">
-        <img src="img\deadstar\logo 1 t.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="img\deadstar\logo 1 t.png" width="30" height="30" class="d-inline-block align-top" alt="deadstar logo">
         deadstar cheats
       </a>
-    </nav>
+      <div class="menu-items navbar-menu">
+        <ul class="menubar">
+          <li class="menu-items"><a href="home.php">HOME</a></li>    
+          <li class="menu-items"><a href="ticket.php">TICKET</a></li>    
+          <li class="menu-items"><a href="profile.php">PROFILE</a></li>    
+          <li class="menu-items" target="_blank" href="https://discord.gg/VjEAzun" style="word-spacing: 0px;"><a href="/buy-subcribe.php">BUY SUBSCRIBE</a></li>    
+          <li class="menu-items"><a target="_blank" href="https://discord.gg/VjEAzun">DISCORD</a></li>    
+          <li class="menu-items" style="
+                                        background-color: #7910a8;
+                                        border: black 9px;
+                                        border-radius: 79px 79px 79px 79px;
+                                        -moz-border-radius: 79px 79px 79px 79px;
+                                        -webkit-border-radius: 50px 50px 50px 50px;
+                                        border: 1px solid rgba(0, 0, 0, 0.212);
+                                        padding: 3px;
+                                        font-size: 11px;
+                                        font-weight: 500;
+                                      "><a href="logout.php">LOGOUT</a></li>    
+        </ul>
+      </div>
+    </div>
+</nav>
